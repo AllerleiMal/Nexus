2ccac12f64354dad1d75cf898115c99856e779e5
AllerleiMal <emelpavel2002@gmail.com>
2023-04-19 01:31:09 +0300

    feat: add login page view

--- BlogApp/BlogApp/Program.cs
+++ BlogApp/BlogApp/Program.cs
@@ -26,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Login}/{id?}");
 
 app.Run();
