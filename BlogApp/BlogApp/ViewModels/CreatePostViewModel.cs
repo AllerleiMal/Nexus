0343ba09f2afc9a132e9dc7f4a6e6aceee0791ce
AllerleiMal <emelpavel2002@gmail.com>
2023-05-09 21:39:59 +0300

    feat: add create post viewmodel and photo validation

---
+++ BlogApp/BlogApp/ViewModels/CreatePostViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BlogApp.DataAttributes;
+
+namespace BlogApp.ViewModels;
+
+public class CreatePostViewModel
+{
+    [DataType(DataType.Upload)]
+    [MaxFileSize(5* 1024 * 1024)]
+    [AllowedExtensions(new string[] { ".jpg", ".png" })]
+    public IFormFile PreviewImage { get; set; }
+}
