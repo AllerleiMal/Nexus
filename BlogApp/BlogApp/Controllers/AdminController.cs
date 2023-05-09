e4e95eba344b19b823c98450ed10da38b135261f
AllerleiMal <emelpavel2002@gmail.com>
2023-05-09 21:38:39 +0300

    feat: move admin action to separate controller

---
+++ BlogApp/BlogApp/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using BlogApp.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Controllers;
+
+public class AdminController : Controller
+{
+    private BlogContext _context;
+
+    public AdminController(BlogContext context)
+    {
+        _context = context;
+    }
+
+    [Authorize(Roles = "admin")]
+    public async Task<IActionResult> UserList()
+    {
+        return View(await _context.Users.ToListAsync());
+    }
+
+    public IActionResult Requests()
+    {
+        var postsPendingReview = _context.Posts
+            .Include(post => post.Blog)
+            .Include(post => post.Tags)
+            .Where(post => post.ModerationPassed == false)
+            .ToListAsync();
+        return View(postsPendingReview);
+    }
+}
