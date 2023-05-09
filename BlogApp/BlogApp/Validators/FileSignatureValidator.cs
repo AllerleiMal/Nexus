85856cd6da253f917be3c5bad06999e51847252e
AllerleiMal <emelpavel2002@gmail.com>
2023-05-09 21:39:59 +0300

    feat: add create post viewmodel and photo validation

---
+++ BlogApp/BlogApp/Validators/FileSignatureValidator.cs
@@ -0,0 +1,42 @@
+﻿namespace BlogApp.DataAttributes;
+
+public class FileSignatureValidator
+{
+    public static bool IsFileValid(IFormFile file)
+    {
+        using (var reader = new BinaryReader(file.OpenReadStream()))
+        {
+            var signatures =
+                FileSignatures.Values.SelectMany(x => x).ToList();
+            var headerBytes = reader.ReadBytes(FileSignatures.Max(m => m.Value.Max(n => n.Length)));
+            bool result = signatures.Any(signature => headerBytes.Take(signature.Length).SequenceEqual(signature));
+            return result;
+        }
+    }
+
+    private static readonly Dictionary<string, List<byte[]>> FileSignatures = new()
+    {
+        { ".gif", new List<byte[]> { new byte[] { 0x47, 0x49, 0x46, 0x38 } } },
+        { ".png", new List<byte[]> { new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A } } },
+        {
+            ".jpeg", new List<byte[]>
+            {
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xE2 },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xE3 },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xEE },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xDB },
+            }
+        },
+        {
+            ".jpg", new List<byte[]>
+            {
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xE0 },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xE1 },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xE8 },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xEE },
+                new byte[] { 0xFF, 0xD8, 0xFF, 0xDB },
+            }
+        }
+    };
+}
