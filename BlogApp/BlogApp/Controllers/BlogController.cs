1558f193a9cedde2b9f7698cd55acf393e0041ec
AllerleiMal <emelpavel2002@gmail.com>
2023-05-11 20:22:21 +0300

    feat: change content span white-space display

--- BlogApp/BlogApp/Controllers/BlogController.cs
+++ BlogApp/BlogApp/Controllers/BlogController.cs
@@ -1,13 +1,33 @@
 ﻿using BlogApp.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Controllers;
 
-public class BlogController
+public class BlogController: Controller
 {
     private BlogContext _context;
 
     public BlogController(BlogContext context)
     {
         _context = context;
+    }
+
+    public IActionResult Details(int? id)
+    {
+        var blog = _context.Blogs
+            .Include(blog => blog.Posts)
+            .ThenInclude(post => post.Tags).FirstOrDefault(blog => blog.Id == id);
+
+        return View(blog);
+    }
+    
+    public IActionResult Post(int? id)
+    {
+        var post = _context.Posts
+            .Include(post => post.Blog)
+            .Include(post => post.Tags)
+            .FirstOrDefault(post => post.Id == id);
+        return View(post);
     }
 }
--- BlogApp/BlogApp/Program.cs
+++ BlogApp/BlogApp/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Post}/{action=CreatePost}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
